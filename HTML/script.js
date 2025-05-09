23fa71ff89a67f01ed1faea3dfde9298ca88d894
MrMArsEl <codechroniclesunderworldtree@gmail.com>
2025-05-09 16:44:16 +0400

    Update script.js

--- HTML/script.js
+++ HTML/script.js
@@ -1,5 +1,6 @@
 <div id="current_date">
     <script>
         document.getElementById("dateHeader").innerHTML = Date();
+        document.getElementById("dateFooter").innerHTML = Date();
     </script>
 </div>
